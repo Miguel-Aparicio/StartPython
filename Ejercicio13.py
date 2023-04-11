66cf26a7c77023a550509782267bf398e27a98c4
Lautaro Rocca <lrocca2019@gmail.com>
2023-04-11 13:37:56 +0200

    eliminar

--- Ejercicio13.py
+++
@@ -1,4 +0,0 @@
-l = int(input("Tamaño del lado: "))
-area = (l**2)*6
-volumen = l**3
-print(f"El area es {area} y el volumen es {volumen}" )
--- Ejercicio15.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-if n < 0:
-    print("Es negativo")
-elif n > 0:
-    print("Es positivo")
-else:
-    print("Es 0")
