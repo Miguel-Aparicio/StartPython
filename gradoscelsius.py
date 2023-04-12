6ee17d6d6cb58a128d9162746f61a926330e27e0
Miguel-Aparicio <miguel.apgd@gmail.com>
2023-04-12 13:40:12 +0200

    Validaciones del 1 al 7

--- gradoscelsius.py
+++ gradoscelsius.py
@@ -1,10 +1,14 @@
 def gradoscelsius():
     # Entrada
-    gradoscelsius = (input ("Ingrese sus grados Celsius: ").replace(",","."))
-    # Proceso
-    try:
-        gradosfahrenheit = (float(gradoscelsius)) * 1.8 + 32
-    # Salida
-        print(f"El equivalente de {round(gradoscelsius,2)} en grados celsius es {round(gradosfahrenheit,2)} en grados grados fahrenheit.")
-    except:
-        print("El número introducido no es correcto.")
+    while True:
+        entrada = (input ("Ingrese sus grados Celsius: ").replace(",","."))
+        # Proceso
+        try:
+            gradoscelsius = float(entrada)
+            gradosfahrenheit = gradoscelsius * 1.8 + 32
+        # Salida
+            res = (f"El equivalente de {round(gradoscelsius,2)} en grados celsius es {round(gradosfahrenheit,2)} en grados grados fahrenheit.")
+            break
+        except:
+            print("El número introducido no es correcto.")
+    print(res)
--- mayormenornum.py
+++ mayormenornum.py
@@ -1,22 +1,23 @@
 def mayormenornum():
     # Entrada
-    lista1 = (input("Ingrese una lista de números separados por espacios: ").split(" "))
-    listanum = []
+    while True:
+        lista1 = (input("Ingrese una lista de números separados por espacios: ").split(" "))
+        listanum = []
     # Proceso
-    try:
-        for i in range(len(lista1)):
-            listanum.append(int(lista1[i]))
-        mayornum = listanum[0]
-        menornum = listanum[0]
-        for i in range(len(listanum)):
-            if listanum[i]>mayornum:
-                mayornum = listanum[i]
-            else:
-                if listanum[i]<menornum:
-                    menornum = listanum[i]
-        res = "El número mayor es:", mayornum
-        res = "El número menor es:", menornum
-    except:
-        res = "Error, existen números introducidos incorrectos."
+        try:
+            for i in range(len(lista1)):
+                listanum.append(int(lista1[i]))
+            mayornum = listanum[0]
+            menornum = listanum[0]
+            for i in range(len(listanum)):
+                if listanum[i]>mayornum:
+                    mayornum = listanum[i]
+                else:
+                    if listanum[i]<menornum:
+                        menornum = listanum[i]
+            res = f"El número mayor es: {mayornum}, y el número menor: {menornum}"
+            break
+        except:
+            print("Error, existen números introducidos incorrectos. Intentelo nuevamente.")
     # Salida
     print(res)
