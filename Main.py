969bd95b1b14f7c0d47f22ffae7e25de1f4679c3
Vitio11 <129160425+Vitio11@users.noreply.github.com>
2023-04-12 10:02:17 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,96 @@
+import os
+#from varios import principal
+import calculoDNI
+import salariobase
+import radiocirculo
+import mayormenornum
+import gradoscelsius
+import numeropar
+import anyobisiesto
+import palindromo
+import listaordenada
+import Ejercicio11
+import Ejercicio12
+import Ejercicio13
+import Ejercicio14
+import Ejercicio15
+import Ejercicio16
+import Ejercicio17
+import Ejercicio18
+import Ejercicio19
+import Ejercicio20
+
+importlibimport
+
+while True
+    os.system('cls') #limpia pantalla
+    print('Bienvenidos')
+    print('1- Determinar letra del DNI')
+    print('2-Calcular salario neto de un empleado')
+    print('3-NO DISPONIBLE')
+    print('4-Calcular area y perimetro del circulo a partir de su radio')
+    print('5-Obtener el numero mayor y menor de una lista')
+    print('6-Conversor de grados Celsius a Farenheit')
+    print('7-Determinar si un numero entero es par o impar')
+    print('8-Determinar si un año es bisiesto o no')
+    print('9-Determina si un texto es palindromo')
+    print('10-Ordenar nombres por orden alfabético')
+    print('11-Calcular el factorial de un número entero')
+    print('12-Determinar si un número es primo o no')
+    print('13-Calcular área y volumen de un cubo')
+    print('14-Sumar todos los nuemros pares de una lista')
+    print('15-Determinar si un numero es positivo, negativo o cero)')
+    print('16-Calcular la media de una lista de números')
+    print('17- Juego: adivina un número del 0 al 100')
+    print('18-Determina si dos cadenas de texto son anagramas')
+    print('19-Eliminar números duplicados de una lista')
+    print('20- Determinar si un número es capicúa')
+    print('0- Salir del programa')
+    opcion = input('seleccione programa a ejecutar')
+     if opcion = '1':
+       calculoDNI()
+     elif opcion = '2'
+       salariobase()
+     elif opcion = '3'
+       
+     elif opcion = '4'
+       radiocirculo()
+     elif opcion = '5'
+       mayormenornum()
+     elif opcion = '6'
+       gradoscelsius()
+     elif opcion = '7'
+       numeropar()
+     elif opcion = '8'
+       anyobisiesto()
+     elif opcion = '9'
+       palindromo()
+     elif opcion = '10'
+       listaordenada()
+     elif opcion = '11'
+       Ejercicio11()
+     elif opcion = '12'
+       Ejercicio12()
+     elif opcion = '13'
+       Ejercicio13()
+     elif opcion = '14'
+       Ejercicio14()
+     elif opcion = '15'
+       Ejercicio15()
+     elif opcion = '16'
+       Ejercicio16()
+     elif opcion = '17'
+       Ejercicio17()
+     elif opcion = '18'
+        Ejercicio18()
+     elif opcion = '19'
+        Ejercicio19()
+     elif opcion = '20'
+       Ejercicio20()
+     elif opcion = 0 
+        print('Un placer, hasta la proxima')
+        break
+    continuar= input('presione enter para continuar')
+
+
+  
